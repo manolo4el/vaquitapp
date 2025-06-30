a2c88ef1e56ec6a024a5a354a1447d70a57fb71c
manolo4el <munassian@gmail.com>
2025-06-29 23:32:10 -0300

    chore: update tailwind.config.ts content paths and clean config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
     "*.{js,ts,jsx,tsx,mdx}",
   ],
